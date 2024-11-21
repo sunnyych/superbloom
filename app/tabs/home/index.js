0fadf3e40b77d52932df69b502ef706173560f23
felicia-yan <feliciay@stanford.edu>
2024-11-20 17:59:24 -0800

    replace svg with png lol
    svgs won't work with rn image components

--- app/tabs/home/index.js
+++ app/tabs/home/index.js
@@ -5,3 +5,23 @@
 // on click add button, navigate to newpost.js
 
 // on click the toggle, navigate to collage.js
+
+import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  FlatList,
+  Image,
+  ImageBackground,
+} from "react-native";
+import Svg, { Path } from "react-native-svg";
+
+export default function HomeGarden() {
+  return (
+    <ImageBackground
+      source={require("../../assets/backgrounds/background garden.svg")}
+    ></ImageBackground>
+  );
+}
